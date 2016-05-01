2837b08a1a6e5b919535ff2394b0b7e6a530746c
Karl OBrien <karlobrien@gmail.com>
2016-05-01 11:36:17 +0100

    use BFS to calculate shortest path knight moves

---
+++ ChessMove/BFS.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMove
+{
+    public class Node
+    {
+        public int row { get; set; }
+        public int col { get; set; }
+        public int step { get; set; }
+    }
+
+    public class BFS
+    {
+        private Queue<Node> _queue;
+        private int[] movementX = new int[] { 1, 2, 2, 1, -1, -2, -2, 1 };
+        private int[] movementY = new int[] { -2, -1, 1, 2, 2, 1, -1, -2 };
+        private int _rows;
+        private int _cols;
+
+        private bool[][] _visited;
+
+        public BFS(int rows, int cols)
+        {
+            _queue = new Queue<Node>();
+            _rows = rows;
+            _cols = cols;
+            initialiseVisited();
+        }
+
+        private void initialiseVisited()
+        {
+            _visited = new bool[_rows][];
+
+            for (int i = 0; i < _rows; i++)
+            {
+                bool[] inner = new bool[_cols];
+                for (int j = 0; j < _cols; j++)
+                {
+                    inner[j] = false;
+                }
+                _visited[i] = inner;
+            }
+        }
+
+        public int MinMoves(int row, int col, int destRow, int destCol)
+        {
+            Node start = new Node();
+            start.row = row;
+            start.col = col;
+            start.step = 0;
+
+            _queue.Enqueue(start);
+
+            while (_queue.Count != 0)
+            {
+                Node top = _queue.Dequeue();
+
+                if (top.row == destRow && top.col == destCol)
+                    return top.step;
+
+                for (var k = 0; k < 8; k++)
+                {
+                    if (top.row + movementY[k] >= 0 && top.row + movementY[k] < _rows &&
+                        top.col + movementX[k] >= 0 && top.col + movementX[k] < _cols)
+                    {
+                        var newRow = top.row + movementY[k];
+                        var newCol = top.col + movementX[k];
+
+                        Node node = new Node() { row = newRow, col = newCol, step = top.step + 1 };
+                        PushToQueue(node);
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private void PushToQueue(Node v)
+        {
+            if (_visited[v.row][v.col]) return;
+            _queue.Enqueue(v);
+            _visited[v.row][v.col] = true;
+        }
+    }
+}
--- ChessMove/Program.cs
+++ ChessMove/Program.cs
@@ -1,15 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChessMove
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var cols = 1000;
+            var rows = 1000;
+            var startCol = 0;
+            var startRow = 1;
+            var endCol = 799;
+            var endRow = 899;
+
+            //var result = GetShortestPath(rows, cols, startRow, startCol, endRow, endCol);
+            //Console.WriteLine("Array Result: " + result);
+            //Console.ReadLine();
+
+            BFS bfs = new BFS(rows, cols);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var bfsResult = bfs.MinMoves(startRow, startCol, endRow, endCol);
+            sw.Stop();
+
+            Console.WriteLine("BFS Result: " + bfsResult + " Time: " + sw.Elapsed);
+            Console.ReadLine();
+
+        }
+
+        public static int GetShortestPath(int rows, int cols, int startRow, int startCol, int endRow, int endCol)
+        {
+            int[][] board = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] inner = new int[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    inner[j] = -1;
+                }
+                board[i] = inner;
+            }
+
+            int[] movementX = new int[] { 1, 2, 2, 1, -1, -2, -2, 1 };
+            int[] movementY = new int[] { -2, -1, 1, 2, 2, 1, -1, -2 };
+
+            var totalSteps = 500;
+            board[startRow][startCol] = 0;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int step = 0; step < totalSteps; step++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (board[i][j] == step)
+                        {
+                            for (var k = 0; k < 8; k++)
+                            {
+                                if (i + movementY[k] >= 0 && i + movementY[k] < rows && j + movementX[k] >= 0 && j + movementX[k] < cols)
+                                    if (board[i + movementY[k]][j + movementX[k]] == -1)
+                                        board[i + movementY[k]][j + movementX[k]] = step + 1;
+                                if (board[endCol][endRow] != -1)
+                                {
+                                    sw.Stop();
+                                    Console.WriteLine(sw.Elapsed);
+                                    return board[endCol][endRow];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        public static void PrintBoard(int[][] board, int rows, int cols)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(board[i][j] + " ");
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
